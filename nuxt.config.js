653b700a97ec9d85b290968f1491eb6fb4f8a76d
Russelo <russeloken@gmail.com>
2022-09-26 14:04:58 +0100

    Configuration

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,14 +11,18 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'kenmoe Russel loic Douala portfolio laravel nuxtjs inertiajs',
+      },
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: ['~/assets/css/main'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
@@ -38,8 +42,13 @@ export default {
     '@nuxtjs/pwa',
     // https://go.nuxtjs.dev/content
     '@nuxt/content',
+    'nuxt-webfontloader',
   ],
-
+  webfontloader: {
+    google: {
+      families: ['Noto+Sans+Mono:wght@300;400&display=swap'],
+    },
+  },
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
     manifest: {
@@ -53,11 +62,31 @@ export default {
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
+    treShake: true,
+    defaultAssets: {
+      font: {
+        family: 'NOTO Sans',
+        size: '15',
+      },
+    },
     theme: {
-      dark: true,
+      light: true,
       themes: {
+        light: {
+          primary: colors.green.darken3,
+          bg: '#ffffff',
+          surface: '#ffffff',
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3,
+        },
         dark: {
-          primary: colors.blue.darken2,
+          primary: colors.green.darken3,
+          bg: '#001E26 ',
+          surface: '#130a24',
           accent: colors.grey.darken3,
           secondary: colors.amber.darken3,
           info: colors.teal.lighten1,
